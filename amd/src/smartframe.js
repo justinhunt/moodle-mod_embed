c386337c93badf9832fbb94f923d376c4992eae8
justinhunt <bitwalkerjapan@gmail.com>
2023-04-30 23:06:33 +0000

    changes

--- amd/src/smartframe.js
+++ amd/src/smartframe.js
@@ -1,4 +1,4 @@
-define(['jquery', 'core/log'], function($, log) {
+define(['jquery', 'core/log','core/ajax'], function($, log,ajax) {
   "use strict"; // jshint ;_;
 
   /*
@@ -9,44 +9,49 @@ define(['jquery', 'core/log'], function($, log) {
 
   return {
 
-    //for making multiple instances
+      //for making multiple instances
       clone: function () {
           return $.extend(true, {}, this);
-     },
+      },
 
-    init: function(itemdata) {
-      this.register_events(itemdata);
-    },
+      init: function (itemdata) {
+          this.register_events(itemdata);
+      },
 
-    register_events: function(itemdata) {
-      
-      var self = this;
+      register_events: function (itemdata) {
+
+          var self = this;
 
 
-      //listen for the grades
-      window.addEventListener("message", function(event) {
-        log.debug('messageevent',event);
+          //listen for the grades
+          window.addEventListener("message", function (event) {
+              log.debug('messageevent', event);
 
-            //if its not really our smartframe host get out of here
-            if (event.origin !== itemdata.smartframehost) {
-                return;
-            }
-            //pass back results and transition
-            var gradesdata = event.data;
-            //each instance of teacher tools itemtype will catch this event, so we need to filter the ones we handle to just this instanc
-           //thats why we pass the itemdata around
-            if(typeof gradesdata != 'undefined' && gradesdata.itemid == itemdata.id) {
-                var thepercent = gradesdata.percent;
+              //if its not really our smartframe host get out of here
+              if (event.origin !== itemdata.smartframehost) {
+                  return;
+              }
 
-                $(".minilesson_nextbutton").prop("disabled", true);
-                setTimeout(function () {
-                    $(".minilesson_nextbutton").prop("disabled", false);
-                    self.next_question(gradesdata.percent);
-                }, 500);
-            }
-          }, false
-      );
-      
-    }
-  };
+              if (event.data.hasOwnProperty(itemdata.smartframetypekey)){
+                  switch (event.data.type) {
+                      case itemdata.smartframegradekey:
+                          //push the grades back to the server
+                      case itemdata.smartframecompletekey:
+                          //push the completion back to the server
+                      case itemdata.smartframereportablekey:
+                        //push the reportable data back to the server
+                          var savedata = {}
+                          $.each(itemdata.smartframedata, function (skey, svalue) {
+                              if (event.data.hasOwnProperty(skey)) {
+                                  savedata[skey] = event.data[skey];
+                              }
+                          });
+                  }
+              }
+
+          });
+      }//end of register events function
+
+  };//end of return
+
 });
